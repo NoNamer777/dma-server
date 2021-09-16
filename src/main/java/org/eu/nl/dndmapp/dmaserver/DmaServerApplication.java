7e9d6526041827bfb21dd485fd6f7fd77385c96c
Oscar Wellner <oscar.wellner@gmail.com>
2021-09-16 15:48:55 +0200

    Initial commit

---
+++ src/main/java/org/eu/nl/dndmapp/dmaserver/DmaServerApplication.java
@@ -0,0 +1,13 @@
+package org.eu.nl.dndmapp.dmaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eu/nl/dndmapp/dmaserver/DmaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eu.nl.dndmapp.dmaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
