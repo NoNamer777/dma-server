c10832114fe954bd9275f785f556c2ac5c1e01e3
Oscar Wellner <oscar.wellner@gmail.com>
2021-09-25 12:43:19 +0200

    Update dependencies

--- src/main/java/org/eu/nl/dndmapp/dmaserver/DmaServerApplication.java
+++ src/main/java/org/eu/nl/dndmapp/dmaserver/DmaServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DmaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DmaServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DmaServerApplication.class, args);
+    }
 }
