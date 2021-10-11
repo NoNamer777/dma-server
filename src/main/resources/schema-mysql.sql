17b18a8e42f05013a3d1fefe084e8cbebe1d2ad9
Oscar Wellner <oscar.wellner@gmail.com>
2021-10-12 00:28:11 +0200

    Modify Schema to drop all found tables generically

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -1,6 +1,26 @@
 USE `dmadb`;
 
-DROP TABLE IF EXISTS `spell`;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET GROUP_CONCAT_MAX_LEN = 32768;
+
+SET @tables = NULL;
+
+SELECT GROUP_CONCAT('`', `table_name`, '`')
+    INTO @tables FROM `information_schema`.`tables`
+    WHERE `table_schema` = (SELECT DATABASE());
+
+SELECT IFNULL(@tables, 'dummy') INTO @tables;
+
+SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
+
+PREPARE statement FROM @tables;
+
+EXECUTE statement;
+
+DEALLOCATE PREPARE statement;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE IF NOT EXISTS `spell` (
     `id` BINARY(16) NOT NULL,
