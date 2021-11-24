749e433ca74552b0f7e6f77ed98e29e473a16992
Oscar Wellner <oscar.wellner@gmail.com>
2021-11-24 20:51:31 +0100

    Add testing workflow

--- src/test/java/org/eu/nl/dndmapp/dmaserver/DmaServerApplicationTests.java
+++ src/test/java/org/eu/nl/dndmapp/dmaserver/DmaServerApplicationTests.java
@@ -2,12 +2,11 @@ package org.eu.nl.dndmapp.dmaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DmaServerApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
